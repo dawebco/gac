8e25b6aba5eeb42722ccdd4a71f9697bac3fae70
dawebco <dawebsiteco@gmail.com>
2026-08-11 22:47:30 +0530

    one

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
-const API_BASE_URL = (process.env.REACT_APP_API_URL || 'http://localhost:4000/api/v1').replace(/\/$/, '');
+const DEFAULT_API_URL = process.env.NODE_ENV === 'production'
+  ? '/api/v1'
+  : 'http://localhost:4000/api/v1';
+const API_BASE_URL = (process.env.REACT_APP_API_URL || DEFAULT_API_URL).replace(/\/$/, '');
 
 let adminCsrfToken = '';
 
